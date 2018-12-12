f5eb5b1949cea38070107eaf962e681561fd1e50
Alexandru Dascalu <alexandru.dascalu94@gmail.com>
2018-12-13 00:49:48 +0200

    [Feature][Android] add updates to gmaps
    Added vibration when notification is received

--- android/BonoPastore/app/src/main/java/com/whitehats/bonopastore/MainActivity.kt
+++ android/BonoPastore/app/src/main/java/com/whitehats/bonopastore/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.whitehats.bonopastore
 
+import android.Manifest
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -13,9 +14,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import android.widget.Toast
 import android.support.v4.app.NotificationCompat
-import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.content.pm.PackageManager
+import android.telephony.TelephonyManager
 import android.app.NotificationChannel
-import android.app.ActivityManager
+
 import android.content.Context
 
 import android.view.View
@@ -27,19 +31,59 @@ import com.google.firebase.iid.FirebaseInstanceId
 import com.whitehats.bonopastore.main.MainView
 import com.whitehats.bonopastore.main.Presenter
 import com.whitehats.bonopastore.main.PresenterImpl
-import com.whitehats.bonopastore.remote.RequestResponse
 import org.json.JSONObject
 import kotlin.properties.Delegates
 import android.app.NotificationManager
+import android.content.DialogInterface
 import android.content.Intent
-import android.app.PendingIntent
-import android.opengl.Visibility
+import android.graphics.Color
+import android.location.Location
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.location.FusedLocationProviderClient
+
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.tasks.OnSuccessListener
+
+import com.google.android.gms.location.places.GeoDataClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.PlaceDetectionClient
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import android.location.LocationManager
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.provider.Settings
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    Response.Listener<JSONObject>, Response.ErrorListener, MainView{
+import com.whitehats.bonopastore.remote.RequestResponse
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+    com.google.android.gms.location.LocationListener,
+    Response.Listener<JSONObject>, Response.ErrorListener, MainView {
 
     var presenter: Presenter by Delegates.notNull()
+    private var mMap: GoogleMap? = null
+
+    private val TAG = "MainActivity"
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private var mLocationManager: LocationManager? = null
+    lateinit var mLocation: Location
+    private var mLocationRequest: LocationRequest? = null
+    private val listener: com.google.android.gms.location.LocationListener? = null
+
+    private val UPDATE_INTERVAL = (2 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
+    private val DEFAULT_ZOOM: Float = 15F
+
+    private var mLocationPermissionGranted = false
+
 
     override fun showNotification(message: String) {
         val channel = NotificationChannel(CHANNEL_ID, channel_name, NotificationManager.IMPORTANCE_HIGH).apply {
@@ -55,17 +99,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             .setAutoCancel(true)
 
         val intent = Intent(this, MainActivity::class.java)
-        val pi = PendingIntent.getActivity(this, 0, intent, Intent.FLAG_ACTIVITY_NEW_TASK)
-        mBuilder.setContentIntent(pi)
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         mNotificationManager.createNotificationChannel(channel)
         mNotificationManager.notify("test",0, mBuilder.build())
+
+        var v :Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        // Vibrate for 500 milliseconds
+        v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
     }
 
     companion object {
         val TAG = "MainActivity"
         val CHANNEL_ID = "0"
         val channel_name = "firebase"
+
+        val PERMISSIONS_PHONE_NUMBER = 2
+        val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 3
     }
 
     var token = ""
@@ -75,7 +124,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         presenter = PresenterImpl(this)
-        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         BonoFirebaseMessagingService.messageListener = presenter
 
 
@@ -107,6 +155,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 Log.d(MainActivity.TAG, token)
                 Toast.makeText(this@MainActivity, token, Toast.LENGTH_SHORT).show()
             })
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+
+        mLocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var mapFragment: MapFragment  = fragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+
+    override fun onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mGoogleApiClient.isConnected) {
+            mGoogleApiClient.disconnect()
+        }
     }
 
     override fun onBackPressed() {
@@ -165,15 +239,84 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             return
         }
 
-        var json = JSONObject()
-        json.put("name", "Alex Dascalu")
-        json.put("phone", "0123456789")
-        json.put("email", "alex@gmail.com")
-        json.put("token", token)
-        RequestResponse.sendJSONRequest(this, this, this, json)
-
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE)
+            != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale
+                      (this, android.Manifest.permission.READ_PHONE_STATE))
+            {
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.READ_PHONE_STATE), PERMISSIONS_PHONE_NUMBER)
+            }
+        }
+        else
+        {
+            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            var json = JSONObject()
+            var simNumber = tm.line1Number
+            if (simNumber == "")
+            {
+                simNumber = tm.subscriberId
+            }
+            json.put("name", "Alex Dascalu")
+            json.put("phone", simNumber)
+            json.put("email", "alex@gmail.com")
+            json.put("token", token)
+            RequestResponse.sendJSONRequest(this, this, this, json)
+        }
 
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_PHONE_NUMBER ->
+            {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    var json = JSONObject()
+                    val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+                    //---get the SIM card ID---
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE)
+                      == PackageManager.PERMISSION_GRANTED)
+                    {
+                        var simNumber = tm.line1Number
+                        if (simNumber == "")
+                        {
+                            simNumber = tm.subscriberId
+                        }
+                        json.put("name", "Alex Dascalu")
+                        json.put("phone", simNumber)
+                        json.put("email", "alex@gmail.com")
+                        json.put("token", token)
+                        RequestResponse.sendJSONRequest(this, this, this, json)
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION ->
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true
+                }
+
+            }
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
 
     override fun onResponse(response: JSONObject?) {
 
@@ -181,5 +324,131 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onErrorResponse(error: VolleyError?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap
+        updateLocationUI()
+    }
+
+    /*
+    * Request location permission, so that we can get the location of the
+    * device. The result of the permission request is handled by a callback,
+    * onRequestPermissionsResult.
+    */
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission( this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED)
+        {
+            mLocationPermissionGranted = true
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+
+        try {
+            if (mLocationPermissionGranted)
+            {
+                mMap?.isMyLocationEnabled = true
+                mMap?.uiSettings?.isMyLocationButtonEnabled = true
+            }
+            else
+            {
+                mMap?.isMyLocationEnabled = false
+                mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                getLocationPermission()
+            }
+        }
+        catch (e: SecurityException)
+        {
+            Log.e("Exception: %s", e.message)
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        Log.i(TAG, "Connection Suspended")
+        mGoogleApiClient.connect()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        var msg = "Updated Location: Latitude " + location.longitude.toString() + location.longitude
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                    // The "which" argument contains the position of the selected item.
+
+            // Add a marker for the selected place, with an info window
+            // showing information about that place.
+        var latLng: LatLng = LatLng(location.latitude, location.longitude)
+        mMap?.addMarker(MarkerOptions()
+            .title("You are here")
+            .position(latLng))
+
+        // Position the map's camera at the location of the marker.
+
+        mMap?.moveCamera( CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM))
+//        circle : Circle = map.addCircle(CircleOptions()
+//        .center()
+//        .radius(10000)
+//        .strokeColor(Color.RED)
+//        .fillColor(Color.BLUE))
+
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+
+            return
+        }
+        startLocationUpdates()
+
+        var fusedLocationProviderClient :
+                FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient .getLastLocation()
+            .addOnSuccessListener(this, OnSuccessListener<Location>
+            { location ->
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    mLocation = location;
+                }
+            })
+    }
+
+    protected fun startLocationUpdates() {
+
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(UPDATE_INTERVAL)
+            .setFastestInterval(FASTEST_INTERVAL)
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+            mLocationRequest, this)
     }
 }
