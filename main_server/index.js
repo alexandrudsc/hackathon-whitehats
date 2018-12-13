6c492520ec404f7daae3a8cd26922b14b450716d
Pamparau Sebastian <pamparau.sebastian@gmail.com>
2018-12-13 19:50:06 +0200

    [Fix][main_server] Enable cors

--- main_server/index.js
+++ main_server/index.js
@@ -17,6 +17,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Connect to Mongoose and set connection variable
 mongoose.connect('mongodb://localhost/resthub', { useNewUrlParser: true });
 
